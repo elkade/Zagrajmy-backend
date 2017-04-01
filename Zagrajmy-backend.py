8f18c157b2cc20bd7db0f934f8cc83b016537c36
elkade <dragan1993@gmail.com>
2017-04-01 20:35:39 +0200

    prosty rest

--- Zagrajmy-backend.py
+++ Zagrajmy-backend.py
@@ -1,4 +1,9 @@
 from flask import Flask
+from flask import abort, jsonify
+import datetime
+import time
+
+from flask import request
 
 app = Flask(__name__)
 
@@ -8,5 +13,41 @@ def hello_world():
     return 'Hello World!'
 
 
+s = "01/12/2011"
+
+matches = [
+    {'id': 1, 'title': "asdfadfgsdfg", 'date': time.mktime(datetime.datetime.strptime(s, "%d/%m/%Y").timetuple())},
+    {'id': 2, 'title': "456wtry", 'date': time.mktime(datetime.datetime.strptime(s, "%d/%m/%Y").timetuple())},
+    {'id': 3, 'title': "po;'0;il;", 'date': time.mktime(datetime.datetime.strptime(s, "%d/%m/%Y").timetuple())},
+    {'id': 4, 'title': "hkjghkhjk", 'date': time.mktime(datetime.datetime.strptime(s, "%d/%m/%Y").timetuple())},
+    {'id': 5, 'title': "q345324", 'date': time.mktime(datetime.datetime.strptime(s, "%d/%m/%Y").timetuple())}
+]
+
+
+@app.route('/matches/<int:match_id>', methods=['GET'])
+def get_match(match_id):
+    match = [match for match in matches if match['id'] == match_id]
+    if len(match) == 0:
+        abort(404)
+    return jsonify(match[0])
+
+
+@app.route('/matches', methods=['GET'])
+def get_matches():
+    return jsonify(matches)
+
+
+@app.route('/matches', methods=['POST'])
+def post_match():
+    match = {
+        'title': request.json.get('title'),
+        'date': request.json.get('date'),
+        'id': max(matches, key=lambda x: x['id'])['id'] + 1
+    }
+
+    matches.append(match)
+    return jsonify(match)
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(threaded=True)
